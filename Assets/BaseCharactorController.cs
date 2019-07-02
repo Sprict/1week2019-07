3eec095687937e807284e756d1cdb933b1cec47e
Sprict <raren0130@gmail.com>
2019-07-02 16:07:17 +0900

    テスト用キャラクターで移動部分完了

---
+++ Assets/BaseCharactorController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseCharactorController : MonoBehaviour
+{
+    private BaseMotionController m_Character;
+
+    private void Awake()
+    {
+        m_Character = GetComponent<BaseMotionController>();
+    }
+    // Update is called once per frame
+    private void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        // 移動
+        m_Character.Move(h, v);
+    }
+}
---
+++ Assets/BaseMotionController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseMotionController : MonoBehaviour
+{
+    private Animator m_Anim;
+    private Rigidbody2D m_Rigidbody2D;
+
+    [SerializeField]
+    private float move_speed = 7.0f;
+
+    private void Awake()
+    {
+        m_Anim = GetComponent<Animator>();
+        m_Rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    public void Move(float h_move, float v_move)
+    {
+        // 移動方向
+        Vector2 direction = new Vector2(h_move, v_move).normalized;
+        transform.Translate(direction * move_speed * Time.deltaTime);
+        m_Anim.SetFloat("Horizontal", h_move);
+        m_Anim.SetFloat("Vertical", v_move);
+    }
+}
