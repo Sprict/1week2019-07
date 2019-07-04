85c082774465bb6a451ff5ad785ad1de2195be38
Sprict <raren0130@gmail.com>
2019-07-04 18:17:25 +0900

    no message

---
+++ Assets/ChoseSpot.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChoseSpot : MonoBehaviour
+{
+    int numSpots = 9;
+    int[] array;
+    // Start is called before the first frame update
+    void Start()
+    {
+        array = new int[4];
+        randomselect4(array, numSpots);
+    }
+
+    void randomselect4(int[] a, int num)
+    {
+        foreach (int e in a)
+        {
+            int rand = Random.Range(1, numSpots + 1);
+            bool isequal = false;
+            foreach (int j in a)
+            {
+                if (rand == j)
+                {
+                    isequal = true;
+                    break;
+                }
+            }
+            if (isequal)
+            {
+                continue;
+            }
+            a[e] = rand;
+        }
+
+        foreach(int i in a)
+        {
+            Debug.Log(a[i]);
+        }
+    }
+
+}
